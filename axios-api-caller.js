93b81284ab51bcc165cd16209317bb78249319d0
Philip Carlsson <philipcarlsson@hotmail.com>
2022-04-07 14:06:11 +0200

    creating first api calls

--- axios-api-caller.js
+++ axios-api-caller.js
@@ -6,3 +6,13 @@ function api() {
     baseURL: process.env.BASE_URL,
   }); //using built in process to reach my env url
 }
+
+//GET all posts
+module.exports.getPosts = async function () {
+  return await api().get("/posts");
+};
+
+//GET a post by id
+module.exports.getPostById = async function (postId) {
+  return await api().get("/posts/" + postId);
+};
--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const express = require("express");
 
 const exphbs = require("express-handlebars");
 
+//api caller via axios
+const api = require("./axios-api-caller.js");
+
 const app = express();
 
 //Setup handlebars
 app.engine(
-  "hbs",
-  hbs.engine({
+  "exphbs",
+  exphbs.engine({
     defaultLayout: "main",
     extname: ".hbs", //so we can name our files .hbs
   })
@@ -18,6 +21,21 @@ app.engine(
 //view through handlebars
 app.set("view engine", "exphbs");
 
-app.listen(8000, () => {
+//testing that call for api calls work
+async function Main() {
+  const postsResponse = await api.getPosts();
+  const posts = postsResponse.data;
+  console.log(posts);
+
+  const postId = posts[0].id;
+  const postResponse = await api.getPostById(postId);
+  const post = postResponse.data;
+  console.log(post);
+}
+
+Main();
+
+/* app.listen(8000, () => {
   console.log("http://localhost:8000/");
 });
+ */
